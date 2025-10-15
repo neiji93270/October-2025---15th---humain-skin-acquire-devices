26a9621066621050db0720774d639d905894067d
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-15 14:34:42 +0200

    Create BestmatchingintermediateParching.hlsl

---
+++ Haskel/BestmatchingintermediateParching.hlsl
@@ -0,0 +1,15 @@
+
+
+
+//the treshold in intermediate devices couldnappear twice
+//in models. sonwe dont need to check the valies aboveb180000 rows.
+//sonwe can use the log fu'ction tontest ill values. then, rk4 method is suited in intermediate energy powered decices.
+//to keep track of futur models, we need to patch them.
+void patchModelsmatrixeswithlowvalues(...)
+
+//a more secure way would be to split decices in skin deformation. then the compiternvision algorithm
+//is the next algorithm in lotterature after the algorithms that are suited to keep track of the bottom right corner 
+//of the cameras images. notice that this id still patching and this is still unsafe.
+//so use intermediate energy devices. haskel transforms
+//would help us cancelling many patches
+void haskelcancelonevalueinonempdel(....)
