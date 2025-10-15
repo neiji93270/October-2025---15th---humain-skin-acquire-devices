36c3124a928a3c4842b74f92544ba2c97434818a
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-15 16:35:09 +0200

    Create DefiorlableSkinHeaders.hlsl

---
+++ Haskel/DefiorlableSkinHeaders.hlsl
@@ -0,0 +1,6 @@
+//as we dont use ui/ux for anlong period and as
+//we dont use the right corne bottom of the camera images, we can use rrace of nxnxn model if we can use 5,7 nulber of theeshold. then everything deals with diffuse sctaering then
+//we need a high range of colors if we use the nxntrace of the mateixes resulting.
+//this mezn that we can use the bottom.right of the images if
+//we can use 5600 values hasardous. the distribution is not gaussian because
+//we cannot use odd values. so we need a model mnist
