d4ef1198c251a8ff517a82aed5bab626f48181c7
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-15 16:28:57 +0200

    Create NonSigma.hlsl

---
+++ Haskel/NonSigma.hlsl
@@ -0,0 +1,12 @@
+//in order to preserve the fist model rows, we need to use a treshold again.
+//this treshold is common. we will use 10^15. this is due to
+//to common log function utility. next, we will use the grace of ghe rows 
+//to get computer vision albedo formulas. this is common to
+//a well known level of integrals if we can use rougness. notice that this os 
+//not ui/ux security.
+
+
+
+//tonget ui/ux security, wr can use containers if
+//it deals with cipher isos. so the isos could be tru'cated
+//by using treshold 5,7 times. this is really ill defined.
